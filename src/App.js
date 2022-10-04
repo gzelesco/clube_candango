410eef249cee7ff680abd43e2c2c21c51c584ece
gzelesco <gzelesco@gmail.com>
2022-10-04 18:19:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Power from './components/power/Power'
+import Speed from './components/speed/Speed'
+import Contact from './components/contact/Contact'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Power />
+      <Speed />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
